d1b1769406877736ee6c884b5ef0347ddf46e7df
developer006tz <developer.ludovic@gmail.com>
2023-03-23 23:12:03 +0300

    solve a minor bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <title>Login</title>
-    <link rel="shortcut icon" href="{{ URL::to('assets/img/favicon.png') }}">
+        <link rel="shortcut icon" href="{{ URL::to('assets/img/favicon/favicon-32x32.png') }}">
     <link rel="stylesheet" href="{{ URL::to('assets/plugins/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ URL::to('assets/plugins/feather/feather.css') }}">
     <link rel="stylesheet" href="{{ URL::to('assets/plugins/icons/flags/flags.css') }}">
